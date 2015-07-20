bc9323c60ac287cd3130a74fb5c40a49b663dad9
jennadawn <j.sampson@mac.com>
2015-07-20 12:27:26 -0400

    Initial Commit

---
+++ HarkTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HarkTest
+//
+//  Created by Jenna Sampson on 7/20/15.
+//  Copyright (c) 2015 Jenna Sampson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
